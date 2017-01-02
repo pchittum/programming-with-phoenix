a6bbd3e181e44d977e7e128f329c9c15f50ea6c9
Peter Chittum <pchittum@salesforce.com>
2017-01-02 22:05:13 +0000

    finished ch 6 authentication

--- web/controllers/user_controler.ex
+++ web/controllers/user_controler.ex
@@ -1,13 +1,17 @@
 defmodule Rumbl.UserController do
 
   use Rumbl.Web, :controller
+
+  plug :authenticate when action in [:index, :show]
+
   alias Rumbl.User
 
   def create(conn, %{"user" => user_params}) do
-    changeset = User.changeset(%User{}, user_params)
+    changeset = User.registration_changeset(%User{}, user_params)
     case Repo.insert(changeset) do
       {:ok, user} ->
         conn
+        |> Rumbl.Auth.login(user)
         |> put_flash(:info, "#{user.name} created!")
         |> redirect(to: user_path(conn, :index))
       {:error, changeset} ->
@@ -22,16 +26,50 @@ defmodule Rumbl.UserController do
     render conn, "new.html", changeset: changeset
   end
 
+  #  BEFORE the Plug function, we had to do this to apply the authenticate
+  # function to this function. But above we use plug and it applies to any we want
+  # def index(conn, _params) do
+  #   case authenticate(conn) do #heavy use of case statments and pattern matching
+  #     #look for halted state in connection and bubble it up
+  #     %Plug.Conn{halted: true} = conn ->
+  #       conn
+  #     #if connection unchanged continue
+  #     conn ->
+  #       users = Repo.all(Rumbl.User)
+  #       render conn, "index.html", users: users
+  #   end
+  # end
   def index(conn, _params) do
-
     users = Repo.all(Rumbl.User)
     render conn, "index.html", users: users
-
   end
 
   def show(conn, %{"id" => id}) do
     user = Repo.get(Rumbl.User, id)
     render conn, "show.html", user: user
   end
+
+  defp authenticate(conn, _opts) do
+    if conn.assigns.current_user do
+      conn
+    else
+      conn
+      |> put_flash(:error, "You must be logged in to access that page")
+      |> redirect(to: page_path(conn, :index))
+      |> halt()
+    end
+  end
+
+  # This version of authenticate is not a plug.
+  # defp authenticate(conn) do
+  #   if conn.assigns.current_user do
+  #     conn
+  #   else
+  #     conn
+  #     |> put_flash(:error, "You must be logged in to access that page")
+  #     |> redirect(to: page_path(conn, :index))
+  #     |> halt()
+  #   end
+  # end
 
 end
--- web/models/user.ex
+++ web/models/user.ex
@@ -10,9 +10,34 @@ defmodule Rumbl.User do
     timestamps
   end
 
+# cast/4 first of all is deprecated! that's fun.
+# but the way it should work is take the model %User{} as the data struct
+# then take the unvalidated params, followed by a string list of required
+# fields...then returns the changeset we can then follow through the pipeline
+# with this changeset and apply other validations...custom validations even
+
   def changeset(model, params \\ :empty) do
     model
     |> cast(params, ~w(name username), [])
+    #|> IO.inspect
     |> validate_length(:username, min: 1, max: 20)
+    #|> IO.inspect
+  end
+
+  def registration_changeset(model, params) do
+    model
+    |> changeset(params)
+    |> cast(params, ~w(password), [])
+    |> validate_length(:password, min: 6, max: 100)
+    |> put_pass_hash()
+  end
+
+  defp put_pass_hash(changeset) do
+    case changeset do
+      %Ecto.Changeset{valid?: true, changes: %{password: pass}} ->
+        put_change(changeset, :password_hash, Comeonin.Bcrypt.hashpwsalt(pass))
+      _ ->
+        changeset
+    end
   end
 end
